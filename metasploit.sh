a92a609561eade68aa950294ad7438c5fce249b0
modapps-lab <68771906+modapps-lab@users.noreply.github.com>
2020-07-29 14:26:14 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -9,12 +9,25 @@ pkg install pv -y
 clear
 toilet "{SUBSCRIBE Mod Apps}" -f term -F border --gay | pv -qL 100
 sleep 1
-toilet "{This IS ONLY 1 TIME BASH URL" -f term -F border --gay | pv -qL 100
+toilet "{This IS ONLY 1 TIME BASH URL}" -f term -F border --gay | pv -qL 100
 sleep 1
-toilet "{NEXT TIME USE THIS COMMAND👇 TO START" -f term -F border --gay | pv -qL 100
+toilet "{NEXT TIME USE THIS COMMAND👇 TO START}" -f term -F border --gay | pv -qL 100
 sleep 1
 toilet "msfconsole" -f term -F border --gay | pv -qL 100
 sleep 8
 pkg install unstable-repo -y
+clear
+toilet "IF YOU GET ERRO OF" -f term -F border --gay | pv -qL 100
+sleep 1
+toilet "E:Unable to locate package unstable-repo" -f term -F border --gay | pv -qL 100
+sleep 1
+toilet "WAIT FOR NEXT VEDIO" -f term -F border --gay | pv -qL 100
+sleep 
+toilet "{START TO INSTALL METASPLOIT}" -f term -F border --gay | pv -qL 100
+sleep 1
+toilet "{IT TAKE TIME TO INSTALL}" -f term -F border --gay | pv -qL 100
+toilet "{PLEASE WAIT}" -f term -F border --gay | pv -qL 100
+sleep 4
+clear
 pkg install metasploit -y
 msfconsole

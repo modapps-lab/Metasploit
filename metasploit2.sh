e9d53e0d167a8c6c5b3cc1018920233eccd4b9b7
modapps-lab <68771906+modapps-lab@users.noreply.github.com>
2020-08-01 13:22:19 +0530

    Create metasploit2.sh

---
+++ metasploit2.sh
@@ -0,0 +1,30 @@
+pkg update -y
+pkg upgrade -y
+apt update -y
+apt upgrade -y
+pkg install toilet -y
+pkg install pv -y
+pkg install ruby -y
+gem install bundler:1.17.3
+pkg install curl -y
+clear
+toilet "PLEASE SUBSCRIBE MY CHANNEL Mod Apps" -f term-F border --gay | pv -qL 100	
+sleep 1
+toilet "YOU WILL NOT GET ANY ERROR" -f term -F border --gay | pv -qL 100	
+sleep 1
+toilet "METASPLOIT IS STATING TO INSTALL" -f term -F border --gay | pv -qL 100	
+sleep 1
+toilet "IT TAKE VERY MUCH TIME TO INSTALL" -f term -F border --gay | pv -qL 100	
+sleep 1
+toilet "PLEASE WAIT" -f term -F border --gay | pv -qL 100	
+sleep 5
+clear
+curl -LO https://github.com/termux/termux-packages/files/3960686/metasploit_5.0.56_android5_all.deb.gz
+gunzip metasploit_5.0.56_android5_all.deb.gz
+dpkg -i metasploit_5.0.56_android5_all.deb
+apt install -f -y
+clear
+toilet "STARTING METASPLOIT" -f term -F border --gay | pv -qL 100	
+toilet "NEXT TIME USE THIS COMMAND TO START👇" -f term -F border --gay | pv -qL 100	
+toilet "msfconsole" -f term -F border --gay | pv -qL 100	
+msfconsole
